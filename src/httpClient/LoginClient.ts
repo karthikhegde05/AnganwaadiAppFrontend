6ef20e3f6b9316722ee02221b7d378fbe6a21633
jishnuvk <jish2000@gmail.com>
2022-03-15 16:03:24 +0530

    integration incomplete

--- src/httpClient/LoginClient.ts
+++ src/httpClient/LoginClient.ts
@@ -4,17 +4,24 @@ export default class LoginClient{
 
     
 
-    static login(id:String, password:String):String{
+    static login(id:String, password:String, fun: Function):void{
         
         
-        Axios.get("http://localhost:8081/login/1").then((response) => {this.loginSuccess(response)}).catch(function (error){console.log(error)});
+        Axios.post("http://localhost:8081/login", {"userID":"hem123"}, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {this.loginSuccess(response, fun)}).catch(function (error){console.log(error)});
         
-        return "string";
     }
 
-    private static loginSuccess(response: AxiosResponse){
+    private static loginSuccess(response: AxiosResponse, fun: Function){
 
-        
+        if(response.data=="valid"){
+            fun();
+        }
+        else
+            alert("wrongPassword");
         console.log(response.data);
 
     }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,44 @@
+import { toastController } from '@ionic/core';
 import { IonContent, 
     IonHeader, 
     IonPage, 
     IonTitle, 
     IonToolbar,
     IonInput,
-    IonButton } from '@ionic/react';
+    IonButton,
+    useIonRouter, 
+    IonGrid,
+    IonRow,
+    IonCol} from '@ionic/react';
 import React, {useState, useEffect} from 'react';
 import LoginClient from '../httpClient/LoginClient';
+import "./Login.css"
 
 
 const Login: React.FC = () => {
 
-    const [username, setUsername] = useState<string>("")
+    const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("")
+    const router = useIonRouter();
+    var pressed:boolean = false;
 
     function loginUser(){ // use an effect instead
-        LoginClient.login("a", "a");
+        // LoginClient.login("a", "a");
+        // router.push("/register");
+        // console.log(pressed);
+        // pressed = true;
+        // console.log(pressed);
+
+       setPassword("a");
+       console.log(password);
+    }
+
+    function moveToHome(){
+      alert("moved to home");
+    }
+
+    async function login(){
+      LoginClient.login(username, password, moveToHome);
     }
     
   return (
@@ -26,12 +49,65 @@ const Login: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonInput placeholder="Username?" onIonChange={(e: any) => setUsername(e.target.value)} />
-        <IonInput placeholder="Password?" onIonChange={(e: any) => setPassword(e.target.value)} />
-        <IonButton onClick={loginUser}>Login</IonButton>
+        
+        <IonGrid>
+          <IonRow class="ion-justify-content-center">
+            <IonCol size='auto'>
+                <IonInput placeholder="Username?" onIonChange={(e: any) => setUsername(e.target.value)} />
+            
+                <IonInput placeholder="Password?" onIonChange={(e: any) => setPassword(e.target.value)} clearInput={true} />
+            </IonCol>
+          </IonRow>
+          
+          <IonRow class="ion-justify-content-center">
+            <IonCol size='auto'>
+            <IonButton disabled={pressed} onClick={login}>Login</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+
       </IonContent>
     </IonPage>
   );
 };
+
+// class Login extends React.Component {
+
+
+
+//   render(): React.ReactNode {
+//       return (    <IonPage>
+//         <IonHeader>
+//           <IonToolbar>
+//             <IonTitle>Anganwaadi App-Login page</IonTitle>
+//           </IonToolbar>
+//         </IonHeader>
+//         <IonContent className="ion-padding">
+          
+//           <IonGrid>
+//             <IonRow class="ion-justify-content-center">
+//               <IonCol size='auto'>
+//                   <IonInput placeholder="Username?" onIonChange={(e: any) => setUsername(e.target.value)} />
+//               </IonCol>
+//             </IonRow>
+  
+//             <IonRow class="ion-justify-content-center">
+//               <IonCol size='auto'>
+//                   <IonInput placeholder="Password?" onIonChange={(e: any) => setPassword(e.target.value)} />
+//               </IonCol>
+//             </IonRow>
+            
+//             <IonRow class="ion-justify-content-center">
+//               <IonCol size='auto'>
+//               <IonButton disabled={pressed} onClick={loginUser}>Login</IonButton>
+//               </IonCol>
+//             </IonRow>
+//           </IonGrid>
+  
+//         </IonContent>
+//       </IonPage>);
+//   }
+
+// }
 
 export default Login;

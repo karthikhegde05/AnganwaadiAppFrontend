e2ee26073981796b90e8f02c58a1a72e5e2d01c3
hemc <hemanthx9@gmail.com>
2022-04-10 17:15:45 +0530

    Added CSS for Worker Page

--- src/pages/WorkerProfilePage.tsx
+++ src/pages/WorkerProfilePage.tsx
@@ -1,13 +1,19 @@
-import {IonButton, IonCol, IonContent, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar} from '@ionic/react';
+import {IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonToggle, IonImg, IonThumbnail, IonText, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle} from '@ionic/react';
 import Axios, { AxiosResponse } from 'axios';
-import { arrowBack, star } from 'ionicons/icons';
+import { arrowBack, callOutline, homeOutline, locationOutline, mailOutline, peopleCircleOutline, peopleOutline, personOutline, star } from 'ionicons/icons';
 import React, {useEffect, useState} from 'react';
+import { useTable } from 'react-table';
 import useAuth from '../hooks/useAuth';
+import styled from 'styled-components';
+import DataTable from 'react-data-table-component';
+import { moon } from "ionicons/icons";
+import styles from "./WorkerProfilePage.module.scss";
 
+import { arrowBackOutline, arrowForward, bookmarkOutline, chatboxEllipsesOutline, ellipsisHorizontal, imageOutline, personAddOutline } from "ionicons/icons";
+import { useLocation } from 'react-router-dom';
 
-const WorkerProfilePage: React.FC = () => {
+const WorkerProfilePage:React.FC = () => {
 
-    
     const [name, setName] = useState<String>("unknown name");
     const [contactNum, setContactNum] = useState<String>("unknown number");
 
@@ -17,14 +23,8 @@ const WorkerProfilePage: React.FC = () => {
 
     const [angAdd, setAngAdd] = useState<String>("unknown address");
     const [angLoc, setAngLoc] = useState<String>("unknown location");
-    
 
     const authContext = useAuth();
-    const logout = async() => {
-        if(authContext!=null){
-            authContext.setAuth({awwId:"", loggedIn:false});
-          }
-    }
 
     const GetDetailsSuccess = (response:AxiosResponse) => {
         if(response.data.nullObj==false){
@@ -48,37 +48,168 @@ const WorkerProfilePage: React.FC = () => {
 
     useEffect(() => {
         GetDetails();
-    }, []); 
+    }, []);
 
-    return (
-        <IonPage>
-            <IonHeader>
-            <IonToolbar>
-                <IonRow>
-                    <IonCol><IonTitle>Worker Profile Page</IonTitle></IonCol>
-                    <IonCol /> <IonCol /> <IonCol />
-                    <IonCol> <IonButton onClick = {logout}>Logout <IonIcon slot="start" icon={arrowBack} /></IonButton></IonCol>
-                </IonRow>
-                
-                
-            </IonToolbar>
-            </IonHeader>
+    const Title = styled(IonTitle)`
+        font-size: 1.5em;
+        text-align: center;
+        color: black;
+    `;
 
-            <IonContent>
-                <h4>Add Anganwaadi worker profile related content here!</h4>
+	return (
 
-                <div>
-                    <p>Name: {name}</p>
-                    <p>Contact number: {contactNum}</p>
-                    <p>username: {username}</p>
-                    <p>email: {email}</p>
-                    <p>role: {role}</p>
-                    <p>anganwaadi address: {angAdd}</p>
-                    <p>anganwaadi location: {angLoc}</p>
-                </div>
-            </IonContent>
-        </IonPage>
-    );
+		<IonPage className={ styles.home }>
+			<IonHeader>
+				<IonToolbar>
+					<IonButtons slot="start">
+
+						<IonButton color="light">
+							<IonIcon icon={ arrowBackOutline } />
+						</IonButton>
+					</IonButtons>
+
+                    <IonCol><Title>Worker Profile Page</Title></IonCol>
+
+					{/*<IonButtons slot="end">
+
+						<IonButton color="light">
+							<IonIcon icon={ ellipsisHorizontal } />
+						</IonButton>
+                    </IonButtons>*/}
+				</IonToolbar>
+			</IonHeader>
+			<IonContent>
+
+				<div className={ styles.topHeader }></div>
+
+				<IonGrid>
+					<IonRow className="ion-justify-content-center">
+						<IonCol size="12" className="ion-justify-content-center ion-align-items-center ion-text-center">
+							<IonCard className={ styles.profileHeader }>
+
+								<IonCardContent>
+
+									<IonRow>
+										<IonCol size="3">
+											<img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/IIIT_Bangalore_Logo.svg/1200px-IIIT_Bangalore_Logo.svg.png" alt="avatar" className={ styles.avatar } />
+										</IonCol>
+                                        <IonCol /><IonCol />
+
+										<IonCol size="8">
+											<IonRow className={ styles.profileInfo }>
+												<IonCol size="12">
+													<IonText color="dark" className={ styles.profileName }>
+														<p>{name}</p>
+													</IonText>
+													<IonText color="medium">
+														<p>Anganwadi Worker</p>
+													</IonText>
+												</IonCol>
+											</IonRow>
+										</IonCol>
+									</IonRow>
+
+									{/*<IonRow>
+										<IonCol size="6">
+											<IonButton fill="outline" expand="block">
+												Message
+											</IonButton>
+										</IonCol>
+
+										<IonCol size="6">
+											<IonButton color="primary" expand="block">
+												<IonIcon icon={ personAddOutline } size="small" />&nbsp;
+												Follow
+											</IonButton>
+										</IonCol>
+                                    </IonRow>*/}
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+					<IonRow className={ styles.profileStatusContainer }>
+						<IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+
+								<IonCardHeader>
+									<IonRow className={ styles.profileStatus }>
+										<IonIcon icon={ personOutline } />
+										<IonCardTitle>{username}</IonCardTitle>
+                                        <IonCardSubtitle>Username</IonCardSubtitle>
+									</IonRow>
+								</IonCardHeader>
+								{/*<IonCardContent>
+									<IonText>
+										<p></p>
+									</IonText>
+                                </IonCardContent>*/}
+							</IonCard>
+						</IonCol>
+
+                        <IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+
+								<IonCardHeader text-wrap>
+									<IonRow className={ styles.profileStatus }>
+										<IonIcon icon={ mailOutline } />
+										<IonCardTitle>{email}</IonCardTitle>
+                                        <IonCardSubtitle>Email</IonCardSubtitle>
+									</IonRow>
+								</IonCardHeader>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+					<IonRow>
+						<IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+								<IonCardContent>
+									<IonIcon icon={ homeOutline } />
+									<IonCardTitle>{angAdd}</IonCardTitle>
+									<IonCardSubtitle>Anganwadi Address</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+
+						<IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+								<IonCardContent>
+									<IonIcon icon={ locationOutline } />
+									<IonCardTitle>{angLoc}</IonCardTitle>
+									<IonCardSubtitle>Anganwadi Location</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+                    <IonRow>
+						<IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+								<IonCardContent>
+									<IonIcon icon={ callOutline } />
+									<IonCardTitle>{contactNum}</IonCardTitle>
+									<IonCardSubtitle>Contact Numbers</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+
+						<IonCol size="6">
+							<IonCard className={ styles.profileCard }>
+								<IonCardContent>
+									<IonIcon icon={ peopleCircleOutline } />
+									<IonCardTitle>{role}</IonCardTitle>
+									<IonCardSubtitle>Role</IonCardSubtitle>
+								</IonCardContent>
+							</IonCard>
+						</IonCol>
+					</IonRow>
+
+				</IonGrid>
+				
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default WorkerProfilePage;
